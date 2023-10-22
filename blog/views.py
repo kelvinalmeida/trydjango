77a4f29d97c25da52f9dd67cfd5aa8889c65235e
kelvin <kelvinsantosdealmeida@gmail.com>
2023-10-21 21:45:13 -0300

    3:27:11

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,22 @@
 from typing import Any
 from django.db import models
+from django.urls import reverse
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import render , get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .forms import ArticleModelForm
 
 from .models import Article
 
+from django.views import View
+
 # Create your views here.
+
+class AboutView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'articles/about.html', {})
+
 
 class ArticleCreateView(CreateView):
     template_name = 'articles/article_create.html'
@@ -19,6 +27,17 @@ class ArticleCreateView(CreateView):
     def form_valid(self, form: BaseModelForm):
         print(form.cleaned_data)
         return super().form_valid(form)
+    
+
+class ArticleUpdateView(UpdateView):
+    template_name = 'articles/article_create.html'
+    queryset = Article.objects.all()
+    context_object_name = 'form'
+    form_class = ArticleModelForm
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
 
 class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
@@ -30,6 +49,21 @@ class ArticleDetailView(DetailView):
     queryset = Article.objects.all()
     context_object_name = 'artic'
 
-    # def get_object(self):
-    #     id_ = self.kwargs.get("id")
-    #     return get_object_or_404(Article, id=id_)
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+    
+
+class ArticleDeleteView(DeleteView):
+    template_name = 'articles/article_delete.html'
+    # queryset = Article.objects.all()
+    context_object_name = 'object'
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+    
+    
+    def get_success_url(self) -> str:
+        return reverse('article:article-list')
+ 
